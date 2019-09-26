6bdeee0e3d451f4513a4bbb48c8a84466c80d401
Phoebe Law <phoebebasilio@gmail.com>
2019-09-26 10:17:20 -0700

    adds examples up to Processing Data

--- src/app.js
+++ src/app.js
@@ -15,12 +15,75 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption));
 app.use(helmet());
+app.use(express.json())
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.send('A GET Request');
 });
 
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send('POST request received.')
+})
+
+app.post('/user', (req, res) => {
+  const { username, password, favoriteClub, newsLetter=false  } = req.query;
+
+  if(!username) { 
+    return res
+      .status(400)
+      .send('Username required')
+  }
+
+  if(!password) { 
+    return res
+      .status(400)
+      .send('Password required')
+  }
+
+  if(!favoriteClub) { 
+    return res
+      .status(400)
+      .send('Favorite club required')
+  }
+
+  if(username.length < 6 || username.length > 20) {
+    return res
+      .status(400)
+      .send('Username must be between 6 and 20 characters')
+  }
+
+  if(password.length < 8 || password.length > 36) {
+    return res
+      .status(400)
+      .send('Password must be between 8 and 36 characters')
+  }
+
+  if (!password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)) {
+    return res
+      .status(400)
+      .send('Password must be contain at least one digit')
+  }
+
+  const clubs = [
+    'Cache Valley Stone Society',
+    'Ogden Curling Club',
+    'Park City Curling Club',
+    'Salt City Curling Club',
+    'Utah Olympic Oval Curling Club'
+  ];
+  
+  // make sure the club is valid
+  if (!clubs.includes(favoriteClub)) {
+    return res
+      .status(400)
+      .send('Not a valid club');
+  }
+
+  res.send('All validation passed')
+
+})
 app.use(function errorHandler(error, req, res, next) {
   let response;
   if (NODE_ENV === 'production') {
--- src/server.js
+++ src/server.js
@@ -3,6 +3,4 @@
 const app = require('./app');
 const { PORT } = require('./config');
 
-const PORT = process.env.PORT || 8000; 
-
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
